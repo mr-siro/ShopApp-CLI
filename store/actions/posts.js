49ab6dd040eeaba3b468073420a4d51e937109c7
nhatmoc97 <nhatmoc97@gmail.com>
2020-03-10 15:28:31 +0700

    add store redux post

--- store/actions/posts.js
+++ store/actions/posts.js
@@ -124,7 +124,7 @@ export const updatePost = (id, title, description, img) => {
             postData: {
                 title,
                 description,
-                imageUrl
+                img
             }
         });
     };
